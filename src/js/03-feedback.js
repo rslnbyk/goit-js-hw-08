d254cda8d623fb16b406e5b2a3f35f2c5f7227c5
rslnbyk <74673598+rslnbyk@users.noreply.github.com>
2022-08-28 12:09:59 +0300

    task 3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import storageApi from './storage';
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = form.querySelector('input[name="email"]');
+const message = form.querySelector('textarea[name="message"]');
+const STORAGE_KEY = 'feedback-form-state';
+loadPage();
+
+form.addEventListener('input', event => {
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  throttle(() => {
+    storageApi.save(STORAGE_KEY, {
+      email: email.value,
+      message: message.value,
+    });
+  }, 500)();
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  storageApi.remove(STORAGE_KEY);
+  console.log({ email: email.value, message: message.value });
+  email.value = '';
+  message.value = '';
+});
+
+function loadPage() {
+  if (storageApi.load(STORAGE_KEY)) {
+    email.value = storageApi.load(STORAGE_KEY).email;
+    message.value = storageApi.load(STORAGE_KEY).message;
+  }
+}
