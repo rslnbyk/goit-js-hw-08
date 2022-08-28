ac9e3ad63c2176bbd49beeb32dd2dc7660bd2b19
rslnbyk <74673598+rslnbyk@users.noreply.github.com>
2022-08-28 08:45:47 +0300

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import storageApi from './storage';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+loadPage();
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    storageApi.save(STORAGE_KEY, data.seconds);
+  }, 1000)
+);
+
+function loadPage() {
+  player.setCurrentTime(storageApi.load(STORAGE_KEY));
+}
